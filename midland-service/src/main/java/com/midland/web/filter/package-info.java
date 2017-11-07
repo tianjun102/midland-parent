62c88f8cf0a7e97e69cd926c51e51c68616e965c
Administrator <977543176@qq.com>
2017-11-07 14:08:03 +0800

    Merge branch 'master' of https://github.com/tianjun102/midland-parent
    # Conflicts:
    #	midland-bs-mannager/src/main/resources/properties/appsetting.properties
    #	midland-service/src/main/java/com/midland/web/SmsSender/SmsClient.java

--- midland-service/src/main/java/com/midland/web/filter/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 过滤器层
- */
-package com.midland.web.filter;
--- midland-web-portal/src/main/java/com/midland/web/controller/WebAppointController.java
+++ midland-web-portal/src/main/java/com/midland/web/controller/WebAppointController.java
@@ -1,12 +1,19 @@
 package com.midland.web.controller;
 
+import com.midland.web.commons.Result;
+import com.midland.web.commons.core.util.ResultStatusUtils;
+import com.midland.web.model.Appointment;
+import com.midland.web.service.AppointmentService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping(value = "/appoint")
 public class WebAppointController {
-    /*@Resource
+    @Resource
     private AppointmentService appointmentServiceImpl;
     @RequestMapping("/rest")
     public Object addAppoint(@RequestBody Appointment obj){
@@ -20,5 +27,5 @@ public class WebAppointController {
             result.setMsg("success");
         }
         return  result;
-    }*/
+    }
 }
