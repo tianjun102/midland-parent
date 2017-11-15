78ff2fa3838ae24ceb2b76a5487c044a5988d059
xiams01 <3332932@qq.com>
2017-11-15 15:03:06 +0800

    Merge branch 'master' of https://github.com/tianjun102/midland-parent
    # Conflicts:
    #	midland-web-portal/src/main/java/com/midland/web/controller/WebUserController.java

--- midland-web-portal/src/main/java/com/midland/web/annocontroller/UserSignUpRestController.java
+++ midland-web-portal/src/main/java/com/midland/web/annocontroller/UserSignUpRestController.java
@@ -26,9 +26,6 @@ public class UserSignUpRestController {
 
     @Resource
     private WebUserService webUserService;
-
-    @Resource
-    private ApiHelper apiHelper;
     @Resource
     private PublicService publicServiceImpl;
 
@@ -54,7 +51,7 @@ public class UserSignUpRestController {
     @RequestMapping("checkVCode")
     public Object checkVCode_(@RequestBody Map map) {
         String phone = (String) map.get("phone");
-        String vCode = (String) map.get("vcode");
+        String vCode = (String) map.get("vCode");
         String key = Contant.SIGN_UP_VCODE_KEY + phone;
         return publicServiceImpl.codeCheck(phone,vCode,key);
     }
