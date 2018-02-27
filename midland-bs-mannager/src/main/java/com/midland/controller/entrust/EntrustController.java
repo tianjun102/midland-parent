ba90762162ba67a520d55cea28b09bbf325ebf4a
xiams01 <3332932@qq.com>
2018-02-27 10:00:18 +0800

    appoint 弹出框

--- midland-bs-mannager/src/main/java/com/midland/controller/entrust/EntrustController.java
+++ midland-bs-mannager/src/main/java/com/midland/controller/entrust/EntrustController.java
@@ -288,8 +288,8 @@ public class EntrustController extends ServiceBaseFilter {
             param.add(obj.getCommunityName()==null?"":obj.getCommunityName());
             param.add(obj.getNickName()==null?"":obj.getNickName());
             param.add(obj.getPhone()==null?"":obj.getPhone());
-           apiHelper.smsSender("18825234000",Contant.SMS_TEMPLATE_89262,param);
-//            apiHelper.smsSender(record.getAgentPhone(),Contant.SMS_TEMPLATE_89262,param);
+//           apiHelper.smsSender("13537670669",Contant.SMS_TEMPLATE_89262,param);
+            apiHelper.smsSender(record.getAgentPhone(),Contant.SMS_TEMPLATE_89262,param);
 
             //发送给预约人的短信：模板id56848，内容：您好！您提交的看房日程由{1}电话{2}帮您带看，该经纪人会尽快联系您安排看房，请保持电话畅通，感谢！
             List<String> param1 = new ArrayList<>();
